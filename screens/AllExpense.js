b9a47145b624bbd5ca87a3d537b6aafe42c21351
Heshan <heshan.imedha@gmail.com>
2022-06-23 12:04:52 +0530

    The Starting Setup

---
+++ screens/AllExpense.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "react-native";
+
+function AllExpense() {
+  return <Text>AllExpense</Text>;
+}
+
+export default AllExpense;
---
+++ screens/ManageExpense.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "react-native";
+
+function ManageExpense() {
+  return <Text>Manage Expense</Text>;
+}
+
+export default ManageExpense;
